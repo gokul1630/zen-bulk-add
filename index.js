53601d25ce324755bd78762f135a1e3d33165db7
gokul1630 <gokulgp3167@gmail.com>
2023-08-10 09:28:55 +0530

    Updated scripts

--- index.js
+++ index.js
@@ -1,66 +1,68 @@
-const fetch = require("node-fetch");
-const xlsx = require("node-xlsx");
-const { readFileSync, writeFileSync, open } = require('fs');
+const fetch = require('node-fetch');
+const readXlsx = require('read-excel-file/node');
+const { writeFileSync, open } = require('fs');
 const path = require('path');
 const dotenv = require('dotenv');
+const schema = require('./schema');
 
-dotenv.config()
+dotenv.config();
 
 const dateObj = (...args) => new Date(...args);
 
 const outputFileName = `output_${dateObj().getDate()}-${dateObj().getMonth()}-${dateObj().getFullYear()}.csv`;
 
-open(outputFileName, 'r', (err, res) => {
-	if (!res) {
-		const outputData = 'name, email, mobile, batch, message\n'
-		writeFileSync(outputFile, outputData, { flag: 'a' });
-		return;
-	}
-	writeFileSync(outputFile, '\n', { flag: 'a' })
-})
-
-const outputFile = path.join(__dirname, outputFileName)
-
-const data = xlsx.parse(readFileSync(__dirname + '/students.xlsx'))[0];
+const outputFile = path.join(__dirname, outputFileName);
 
 const token = process.env.TOKEN;
-const BACKEND_URL = process.env.BACKEND_URL
+const BACKEND_URL = process.env.BACKEND_URL;
 
-/** default value should be 1 */
-const indexResetOffset = 1;
+const indexResetOffset = 0;
 
 (async () => {
-	const batchResponse = await fetch(`${BACKEND_URL}/batch/all-names`, {
-		method: 'GET',
-		headers: { 'Content-Type': 'application/json', authorization: `bearer ${token}`, }
+	try {
+		
+	open(outputFileName, 'r', (err, res) => {
+
+		if (!res) {
+			const outputData = 'name, email, mobile, batch, message\n';
+			writeFileSync(outputFile, outputData, { flag: 'a' });
+			return;
+		}
+		writeFileSync(outputFile, '\n', { flag: 'a' });
 	});
 
-	const batchData = await batchResponse.json()
+    const data = await readXlsx('students.xlsx', { schema });
+    const batchResponse = await fetch(`${BACKEND_URL}/batch/all-names`, {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json', authorization: `bearer ${token}` },
+    });
 
-	for (let index = indexResetOffset; index < data.data.length; index += 1) {
-		const element = data.data[index];
+    const batchData = await batchResponse.json();
 
-		if (element.length && token && BACKEND_URL) {
-			const name = element[4]
-			const email = element[1]
-			const mobile = `${element[5]}`
-			const batch = element[3]
-			const batchName = batchData.batches?.find(key => key?._id === batch)?.name
+    for (let index = indexResetOffset; index < data?.rows.length; index += 1) {
+        const element = data?.rows[index];
 
-			setTimeout(async () => {
-				const response = await fetch(`${BACKEND_URL}/users/student/create`, {
-					method: 'POST',
-					body: JSON.stringify({
-						name, email, batch, mobile
-					}),
-					headers: { 'Content-Type': 'application/json', authorization: `bearer ${token}`, }
+        const { name, email, mobile, batch } = element;
 
-				});
-				const res = await response.json();
-				const outputData = `${name}, ${email}, ${mobile}, ${batchName}, ${res?.message}\n`
-				writeFileSync(outputFile, outputData, { flag: 'a' })
-				console.log(index, res?.message, ' ---> ', batchName, email);
-			}, 1000 * (index - indexResetOffset + 1));
-		}
+        const batchName = batchData?.batches?.find((key) => key?._id === batch)?.name;
+
+        setTimeout(async () => {
+            const response = await fetch(`${BACKEND_URL}/users/student/create`, {
+                method: 'POST',
+                body: JSON.stringify(element),
+                headers: { 'Content-Type': 'application/json', authorization: `bearer ${token}` },
+            });
+
+            const res = await response.json();
+
+            const outputData = `${name}, ${email}, ${mobile}, ${batchName}, ${res?.message}\n`;
+
+            writeFileSync(outputFile, outputData, { flag: 'a' });
+
+            console.log(index + 1, res?.message, ' ---> ', batchName, email);
+        }, 1000 * (index - indexResetOffset));
+    }
+	} catch (error) {
+		console.log(error);
 	}
-})()
+})();
---
+++ schema.js
@@ -0,0 +1,20 @@
+const schema = {
+	'Student Name': {
+		prop: 'name',
+		type: String,
+	},
+	'Student Contact Mobile:': {
+		prop: 'mobile',
+		type: String,
+	},
+	'Course Selected': {
+		prop: 'batch',
+		type: String,
+	},
+	'Email Address': {
+		prop: 'email',
+		type: String,
+	}
+};
+
+module.exports = schema
