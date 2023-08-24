904159f793d9d8f5bac858470e07cccd6557fb2d
gokul1630 <gokulgp3167@gmail.com>
2023-08-24 10:26:01 +0530

    Added batches

--- index.js
+++ index.js
@@ -22,6 +22,12 @@ const token = process.env.TOKEN;
 const BACKEND_URL = process.env.BACKEND_URL;
 
 const indexResetOffset = 0;
+const batches = {
+	"fullstackwithpythonprogramming": "64d36ab4ca58e400768b6887",
+	"chatgpt": "64d36a7aeca69f00773c53f9",
+	"devops": "64d36a93d2077700769bb524",
+};
+
 const programs = {
 	"64d36a7aeca69f00773c53f9": "27",
 	"64d36a93d2077700769bb524": "28",
@@ -52,70 +58,63 @@ const programs = {
 
 		const data = await readXlsx(argv?.mentor ? 'mentors.xlsx' : 'students.xlsx', { schema: argv?.mentor ? mentorSchema : studentSchema });
 
-		let batchData = {};
 		let rolesData = {}
-		if (argv?.mentor) {
-			const batchResponse = await fetch(`${BACKEND_URL}/manageValues/get-user-roles`, {
-				method: 'GET',
-				headers: { 'Content-Type': 'application/json', authorization: `bearer ${token}` },
-			});
-			rolesData = await batchResponse.json();
-
+		if(argv?.console){
+			console.log(JSON.stringify(data.rows.map(k => k?.email)), data.rows.length)
 		} else {
-			const batchResponse = await fetch(`${BACKEND_URL}/batch/all-names`, {
-				method: 'GET',
-				headers: { 'Content-Type': 'application/json', authorization: `bearer ${token}` },
-			});
-
-			batchData = await batchResponse.json();
-		}
-
-		for (let index = indexResetOffset; index < data?.rows.length; index += 1) {
-			const element = data?.rows[index];
-
-			const name = element?.name;
-			const email = element?.email;
-			const batch = element?.batch;
-			const mobile = element?.mobile;
-			const role = element?.role;
-			const primaryEmail = element?.primaryEmail;
-			const secondaryEmail = element?.secondaryEmail;
-			const program = programs[batch] || 0;
-
-
-			const url = argv?.mentor ? `${BACKEND_URL}/users/create` : `${BACKEND_URL}/users/student/create`
-
-			if (!argv?.dry) {
-				setTimeout(async () => {
-					const response = await fetch(url, {
-						method: 'POST',
-						body: argv?.mentor ? JSON.stringify({ name, email: secondaryEmail || primaryEmail, mobile, role }) : JSON.stringify({...element, program}),
-						headers: { 'Content-Type': 'application/json', authorization: `bearer ${token}` },
-					});
-
-					const res = await response.json();
-
-					let outputData
+			if (argv?.mentor) {
+				const batchResponse = await fetch(`${BACKEND_URL}/manageValues/get-user-roles`, {
+					method: 'GET',
+					headers: { 'Content-Type': 'application/json', authorization: `bearer ${token}` },
+				});
+				rolesData = await batchResponse.json();
+			}
+	
+			for (let index = indexResetOffset; index < data?.rows.length; index += 1) {
+				const element = data?.rows[index];
+	
+				const name = element?.name;
+				const email = element?.email;
+				const batch = batches[element?.batch.toLowerCase().replace(/\s/g,'')];
+				const mobile = element?.mobile;
+				const role = element?.role;
+				const primaryEmail = element?.primaryEmail;
+				const secondaryEmail = element?.secondaryEmail;
+				const program = programs[batch] || 0;
+	
+	
+				const url = argv?.mentor ? `${BACKEND_URL}/users/create` : `${BACKEND_URL}/users/student/create`
+	
+				if (!argv?.dry) {
+					setTimeout(async () => {
+						const response = await fetch(url, {
+							method: 'POST',
+							body: argv?.mentor ? JSON.stringify({ name, email: secondaryEmail || primaryEmail, mobile, role }) : JSON.stringify({...element, batch, program}),
+							headers: { 'Content-Type': 'application/json', authorization: `bearer ${token}` },
+						});
+	
+						const res = await response.json();
+	
+						let outputData
+						if (!argv?.mentor) {
+							outputData = `${name}, ${email}, ${mobile}, ${element?.batch}, ${res?.message}\n`;
+							console.log(index + 1, res?.message, ' ---> ', element?.batch, email);
+						} else {
+							const { name: roleName } = rolesData?.roles?.find((key) => key?._id === role);
+							outputData = `${name}, ${secondaryEmail || primaryEmail}, ${mobile}, ${roleName}, ${res?.message}\n`;
+							console.log(index + 1, res?.message, ' ---> ', roleName, secondaryEmail || primaryEmail);
+						}
+	
+						writeFileSync(argv?.mentor ? mentorOutputFile : studentOutputFile, outputData, { flag: 'a' });
+	
+					}, 1000 * (index - indexResetOffset))
+				} else {
 					if (!argv?.mentor) {
-						const { name: batchName } = batchData?.batches?.find((key) => key?._id === batch);
-						outputData = `${name}, ${email}, ${mobile}, ${batchName}, ${res?.message}\n`;
-						console.log(index + 1, res?.message, ' ---> ', batchName, email);
+						console.log(index + 1, element?.batch, program, ' ---> ', email);
 					} else {
 						const { name: roleName } = rolesData?.roles?.find((key) => key?._id === role);
-						outputData = `${name}, ${secondaryEmail || primaryEmail}, ${mobile}, ${roleName}, ${res?.message}\n`;
-						console.log(index + 1, res?.message, ' ---> ', roleName, secondaryEmail || primaryEmail);
+						console.log(index + 1, roleName, ' ---> ', secondaryEmail || primaryEmail);
 					}
-
-					writeFileSync(argv?.mentor ? mentorOutputFile : studentOutputFile, outputData, { flag: 'a' });
-
-				}, 1000 * (index - indexResetOffset))
-			} else {
-				if (!argv?.mentor) {
-					const { name: batchName } = batchData?.batches?.find((key) => key?._id === batch);
-					console.log(index + 1, batchName, program, ' ---> ', email);
-				} else {
-					const { name: roleName } = rolesData?.roles?.find((key) => key?._id === role);
-					console.log(index + 1, roleName, ' ---> ', secondaryEmail || primaryEmail);
 				}
 			}
 		}
